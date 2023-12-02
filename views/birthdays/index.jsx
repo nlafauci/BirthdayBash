3005ddec5be338d9f4d7144cf8b78f9bfa551651
saradonnelly97 <sara.donnelly8@gmail.com>
2023-12-02 11:27:02 -0500

    adjusted index

--- views/birthdays/index.jsx
+++ views/birthdays/index.jsx
@@ -20,31 +20,6 @@ function index (data) {
           </main>
       </Def>
   )
-    return (
-        <div className="col-sm-6" key={birthday.birthdayId}>
-            <h2>
-                <a href="#" onClick={() => history.push(`/places/${birthday.birthdayId}`)} >
-                    {birthday.name}
-                </a>
-            </h2>
-            <p className="text-center">
-                {place.cuisines}
-            </p>
-            <img style={{ maxWidth: 200 }} src={place.pic} alt={place.name} />
-            <p className="text-center">
-                Located in {place.city}, {place.state}
-            </p>
-        </div>
-    )
-})
-return (
-    <main>
-        <h1>Birthday</h1>
-        <div className="row">
-            {placesFormatted}
-        </div>
-    </main>
-)
   }
   
 
