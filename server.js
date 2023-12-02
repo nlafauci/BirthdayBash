222c807d759289abefb68895aa0c33f79f6c8a7d
ALISON SALINAS <gisellsalinas43@gmail.com>
2023-12-02 13:10:01 -0500

    configured controllers in to server

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express')
 require('dotenv').config()
 const methodOverride = require('method-override')
 const mongoose = require('mongoose')
+const birthdayController = require('./controllers/BirthdayController')
+const userController = require('./controllers/UserController')
 
 const app = express()
+
 
 // Middleware (express settings)
 app.use(express.urlencoded({ extended: true }))
@@ -12,6 +15,20 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(methodOverride('_method'))
+
+//CONTROLLERS
+app.use('/birthdays', birthdayController)
+app.use('/users', userController)
+
+
+// BASIC ROUTES
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('*', (req, res) => {
+    res.status(404).render('error404')
+})
 
 // MONGOOSE DATABASE CONNECTION
 mongoose.connect(process.env.MONGO_URI, {
